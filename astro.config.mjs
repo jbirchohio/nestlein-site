59cbc0ca837bc6a652a587eb07518ba2f085b6fb
jbirchohio <55216724+jbirchohio@users.noreply.github.com>
2025-04-08 17:12:28 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({});
- 
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [tailwind()]
+});
