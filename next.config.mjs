50792bab574c24af56eca224449b32049f274925
jbirchohio <55216724+jbirchohio@users.noreply.github.com>
2025-04-18 23:36:30 -0400

    asd

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 import mdx from '@next/mdx';
 
 const withMDX = mdx({
-  extension: /\.mdx?$/
+  extension: /\.mdx?$/,
 });
 
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
-    unoptimized: true
+    // Remove this line:
+    // unoptimized: true,
+    // Add this instead:
+    domains: ['lh3.googleusercontent.com'],
   },
-  };
+};
 
 export default withMDX(nextConfig);
