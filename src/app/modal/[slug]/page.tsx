5a0b2e888ad89a2ac9ece65e61d2709bad4e5a0d
jbirchohio <55216724+jbirchohio@users.noreply.github.com>
2025-04-20 21:43:04 -0400

    Update page.tsx

--- src/app/modal/[slug]/page.tsx
+++ src/app/modal/[slug]/page.tsx
@@ -3,9 +3,8 @@
 import { useRouter } from 'next/navigation'
 import Modal from '@/components/Modal'
 import LocationDetail from '@/components/LocationDetail'
-import type { PageProps } from 'next'
 
-export default function LocationModal({ params }: PageProps<{ slug: string }>) {
+export default function LocationModal({ params }: { params: { slug: string } }) {
   const router = useRouter()
 
   return (
