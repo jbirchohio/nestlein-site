085bb85b636250eeab244c8334dd22bb4fc82363
jbirchohio <55216724+jbirchohio@users.noreply.github.com>
2025-04-22 13:50:38 -0400

    breh
    breh

--- src/app/map/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const MapPageClient = dynamic(() => import('@/components/MapPageClient'), { ssr: false });
-
-export default function Page() {
-  return <MapPageClient />;
-}
