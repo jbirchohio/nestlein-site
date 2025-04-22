ea83f9afe87abaddb295a1518403ecc06d2894a2
jbirchohio <55216724+jbirchohio@users.noreply.github.com>
2025-04-21 23:57:28 -0400

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,11 @@
 // app/(home)/page.tsx
-import ClientHome from '@/components/ClientHome';
+import dynamic from 'next/dynamic';
+
+// dynamically import your ClientHome _only_ on the client
+const ClientHome = dynamic(() => import('@/components/ClientHome'), {
+  ssr: false,
+});
 
 export default function Page() {
   return <ClientHome />;
 }
- 
